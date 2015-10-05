c9151bb99a69f4b81e684d0e55ad0adc4c743cb3
cwholmes <cholmes5324@gmail.com>
2015-10-04 21:03:36 -0500

    Create checkLogin.php

---
+++ checkLogin.php
@@ -0,0 +1,35 @@
+<?php
+$conn = mysqli_connect("176.32.230.252","cl57-xuezheng","HnsXB/zKk","cl57-xuezheng");
+// Check connection
+if(!$conn) {
+    die('Connection problem ! --> '.mysql_error());
+}
+
+$email = $_POST['email']; 
+$password = $_POST['password'];
+$tablename = 'users';
+
+$sql="SELECT * FROM $tablename WHERE email='$email'";
+$result=mysqli_query($conn, $sql);
+
+$count=mysqli_num_rows($result);
+// If result matched $email, table row must be 1 row
+if($count==1){
+    $row = mysqli_fetch_assoc($result);
+    if ($password == $row['password']){
+        //session_register("email");
+        //session_register("password"); 
+        echo "Login Successful";
+        return true;
+    }
+    else {
+        echo "Wrong email or Password";
+        return false;
+    }
+}
+else{
+    echo "Wrong email or Password";
+    return false;
+}
+
+?>
