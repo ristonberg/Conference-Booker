67c2f9e00fb00a58388160c6eb7eaca90019659e
cwholmes <cholmes5324@gmail.com>
2015-10-04 19:48:44 -0500

    Update Dbconnect.php

--- Dbconnect.php
+++ Dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-if(!mysql_connect("localhost","root",""))
+if(!mysql_connect("176.32.230.252","cl57-xuezheng","HnsXB/zKk","cl57-xuezheng"))
 {
      die('Connection problem ! --> '.mysql_error());
 }
