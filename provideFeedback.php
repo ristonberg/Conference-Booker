20f9d0d3906ceef83d68c2eaba9eff0eb424f228
cwholmes <cholmes5324@gmail.com>
2015-11-20 12:41:26 -0600

    Update provideFeedback.php

--- provideFeedback.php
+++ provideFeedback.php
@@ -34,6 +34,8 @@ textarea:required:invalid, input:required:invalid {
 }
 </style>
 <script type="text/javascript" src="logOut.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+<link rel="stylesheet" href="stars.css">
 <link rel="stylesheet" href="welcome.css">
 
 </head>
@@ -158,14 +160,19 @@ document.write(myMessage);
 			</select>
 		</div>
 <label for="overall">Rate your overall experience: </label>
-<select id="overall" name="overall" required>
-<option value="0">0</option>
-<option value="1">1</option>
-<option value="2">2</option>
-<option value="3">3</option>
-<option value="4">4</option>
-<option value="5">5</option>
-</select>
+
+<div class="stars">
+    <input class="star star-5" id="star-5" type="radio" name="overall" value="5"/>
+    <label class="star star-5" for="star-5"></label>
+    <input class="star star-4" id="star-4" type="radio" name="overall" value="4"/>
+    <label class="star star-4" for="star-4"></label>
+    <input class="star star-3" id="star-3" type="radio" name="overall" value="3"/>
+    <label class="star star-3" for="star-3"></label>
+    <input class="star star-2" id="star-2" type="radio" name="overall" value="2"/>
+    <label class="star star-2" for="star-2"></label>
+    <input class="star star-1" id="star-1" type="radio" name="overall" value="1"/>
+    <label class="star star-1" for="star-1"></label>
+</div>
 <br><br>
 
 <label for="comments">Please write some comments: </label><br>
